d41a163a742512d9d1c99cdce4a3166ff94a0d9b
TRINAY MITHRA <trinaymithra@gmail.com>
2021-06-22 13:56:49 +0530

    Resolved Errors

--- MinHeap.cpp
+++ MinHeap.cpp
@@ -16,20 +16,20 @@ public:
 
     void heapify(int i); // To heapify subtree rooted with index i
 
-    int parent(int i) { //return index of parent of i 
-        return (i - 1) / 2; 
+    int parent(int i) { //return index of parent of i
+        return (i - 1) / 2;
     }
 
     int leftchild(int i) { //return index of left child of i
-        return (2 * i + 1); 
+        return (2 * i + 1);
     }
     int rightchild(int i) { //return right child of i
-        return (2 * i + 2); 
+        return (2 * i + 2);
     }
- 
+
     int delete_minimum(); // extracts root (minimum) element
-    int getMin() { 
-        return harr[0]; 
+    int getMin() {
+        return heap[0];
     } // Returns minimum
 
     void insert_ele(int element);
@@ -47,18 +47,18 @@ MinHeap::MinHeap(int a[], int size) { //convert the array into heap
         i--;
     }
 }
- 
+
 void MinHeap::heapify(int i) {
     int l = leftchild(i);
     int r = rightchild (i);
     int smallest = i;
 
-    if (l < heap_size && harr[l] < harr[smallest])
+    if (l < heap_size && heap[l] < heap[smallest])
         smallest = l;
-    if (r < heap_size && harr[r] < harr[smallest])
+    if (r < heap_size && heap[r] < heap[smallest])
         smallest = r;
-    if (smallest != i) { //one of the child is smaller than the parent 
-        swap(&harr[i], &harr[smallest]);//swap that child with parent
+    if (smallest != i) { //one of the child is smaller than the parent
+        swap(&heap[i], &heap[smallest]);//swap that child with parent
         heapify(smallest);
     }
 }
@@ -66,7 +66,7 @@ void MinHeap::heapify(int i) {
 void MinHeap::insert_ele(int element) {
     if (heap_size == capacity) {
         cout<<"Heap is full";
-        return heap;
+        return;
     }
 
     heap_size++;
@@ -74,8 +74,8 @@ void MinHeap::insert_ele(int element) {
 
     int curr_index = heap_size - 1;
 
-    while (curr_index != 0 && heap[parent(i)] > heap[i]) {
-       swap(&harr[i], &harr[parent(i)]);
+    while (curr_index != 0 && heap[parent(curr_index)] > heap[curr_index]) {
+       swap(&heap[curr_index], &heap[parent(curr_index)]);
        curr_index = parent(curr_index);
     }//insert and restore the rules of heap
 
@@ -96,11 +96,11 @@ int MinHeap::delete_indexed_element(int index) {
 
     int root = heap[index];
     heap[index] =getMin() - 1;
-    
+
     int curr_index = index;
 
-    while (curr_index != 0 && heap[parent(i)] > heap[i]) {
-       swap(&harr[i], &harr[parent(i)]);
+    while (curr_index != 0 && heap[parent(curr_index)] > heap[curr_index]) {
+       swap(&heap[curr_index], &heap[parent(curr_index)]);
        curr_index = parent(curr_index);
     }
 
@@ -113,7 +113,7 @@ int MinHeap::delete_element(int element) {
     int i, root;
     for (i = 0; i<heap_size; i++)
         if (heap[i] == element) {
-            root = delete_indexed_element(i)
+            root = delete_indexed_element(i);
         }
     return root;
 }
@@ -122,6 +122,20 @@ void MinHeap::print_heap() {
     int i;
     for (i = 0; i<heap_size; i++)
         cout<<heap[i]<<"\t";
+    cout<<"\n";
 }
 
- 
+int main() {
+    int arr[] = {1,4,3,7,2,8};
+
+    MinHeap mh(arr,sizeof(arr)/sizeof(arr[0]));
+    mh.print_heap();
+    mh.delete_element(2);
+    mh.print_heap();
+    mh.insert_ele(10);
+    mh.print_heap();
+
+
+    return 0;
+}
+
